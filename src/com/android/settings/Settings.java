3a217879779836794818cfbd97dbe287676fdcf9
Nathan Chancellor <natechancellor@gmail.com>
2017-03-06 15:43:00 -0500

    Add Magisk Manager dashboard option
    The primary reason for bringing this into the dashboard is that the
    Magisk Manager will soon have root management options, meaning that
    people will no longer need the Superuser app
    
    I would have liked to use the actual Magisk icon from within the app
    but it just does not look good at lower DPIs so I picked a tuning icon
    from https://materialdesignicons.com/
    
    Preview with summary: http://i.imgur.com/Zc1FJN8.png
    
    Signed-off-by: Varun Date <date.varun123@gmail.com>

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -162,4 +162,7 @@ public class Settings extends SettingsActivity {
     public static class DeviceSettings extends SettingsActivity { /* empty */ }
     public static class PersonalSettings extends SettingsActivity { /* empty */ }
     public static class SystemSettings extends SettingsActivity { /* empty */ }
+
+    // Dashboard
+    public static class MagiskActivity extends SettingsActivity { /* empty */ }
 }
