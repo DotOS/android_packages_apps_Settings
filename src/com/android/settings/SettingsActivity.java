5c607dc67ebd4a36571ce47f669e1ee523b9997d
ganeshi4u <ganeshvarmai4u@gmail.com>
2017-07-23 11:23:13 +0530

    fix derp
    * missing semicolon

--- src/com/android/settings/SettingsActivity.java
+++ src/com/android/settings/SettingsActivity.java
@@ -377,7 +377,7 @@ public class SettingsActivity extends SettingsDrawerActivity
             MasterClear.class.getName(),
             NightDisplaySettings.class.getName(),
             ManageDomainUrls.class.getName(),
-            AutomaticStorageManagerSettings.class.getName()
+            AutomaticStorageManagerSettings.class.getName(),
 	    dotSettings.class.getName(),
     };
 
