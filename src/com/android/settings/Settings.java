c7896f0311dd6b6c228ed3fc1b1c3793c2752d71
maxwen <max.weninger@gmail.com>
2017-09-08 05:07:18 +0530

    Build DotExtras into Settings   * Add it on dashboard   * Set proper priority to show it below Apps & Notifications
    Change-Id: If65ceefd35577c8287c0522e7bce329afd37f270
    Signed-off-by: Mohan CM <mohanmanjappa@gmail.com>

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -191,4 +191,7 @@ public class Settings extends SettingsActivity {
     public static class UserAndAccountDashboardActivity extends SettingsActivity {}
     public static class SystemDashboardActivity extends SettingsActivity {}
 
+    // dotEXTRAS
+    public static class DotExtrasSettingsActivity extends SettingsActivity {}
+
 }
--- src/com/android/settings/core/gateway/SettingsGateway.java
+++ src/com/android/settings/core/gateway/SettingsGateway.java
@@ -132,6 +132,8 @@ import com.android.settings.wifi.WifiInfo;
 import com.android.settings.wifi.WifiSettings;
 import com.android.settings.wifi.p2p.WifiP2pSettings;
 
+import com.dot.dotextras.DotExtrasFragment;
+
 public class SettingsGateway {
 
     /**
@@ -252,7 +254,8 @@ public class SettingsGateway {
             WebViewAppPicker.class.getName(),
             LockscreenDashboardFragment.class.getName(),
             BluetoothDeviceDetailsFragment.class.getName(),
-            DataUsageList.class.getName()
+            DataUsageList.class.getName(),
+            DotExtrasFragment.class.getName(),
     };
 
     public static final String[] SETTINGS_FOR_RESTRICTED = {
@@ -290,5 +293,6 @@ public class SettingsGateway {
             Settings.DateTimeSettingsActivity.class.getName(),
             Settings.DeviceInfoSettingsActivity.class.getName(),
             Settings.EnterprisePrivacySettingsActivity.class.getName(),
+            Settings.DotExtrasSettingsActivity.class.getName(),
     };
 }
