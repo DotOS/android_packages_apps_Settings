99a697896b9530a8791dcb754144d727bf5136a8
maxwen <max.weninger@gmail.com>
2017-12-20 21:56:45 +0000

    [2/2] Settings: configure SystemUI theme
    allow to force light or dark or use the wallpaper based
    automatic selection
    
    [Kshitij Gupta]
    - Add a preference controller so as to adapt code for
      Display Settings
    - Refactor some variables. We're theming System, not just
      SystemUI
    
    Change-Id: I40fc1ea4ce6026bb381a30fd4e039a2d10ecfa3a
    Signed-off-by: Kshitij Gupta <kshitijgm@gmail.com>

---
+++ src/com/android/settings/display/DarkUIPreferenceController.java
@@ -0,0 +1,73 @@
+/*
+ * Copyright (C) 2017 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the
+ * License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+package com.android.settings.display;
+
+import android.content.Context;
+import android.content.ContentResolver;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.support.v7.preference.ListPreference;
+import android.support.v7.preference.Preference;
+import android.support.v7.preference.PreferenceScreen;
+import android.provider.Settings;
+
+import com.android.settingslib.core.AbstractPreferenceController;
+
+import libcore.util.Objects;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class DarkUIPreferenceController extends AbstractPreferenceController implements
+        Preference.OnPreferenceChangeListener {
+
+    private static final String SYSTEM_THEME_STYLE = "system_theme_style";
+    private ListPreference mSystemThemeStyle;
+
+    public DarkUIPreferenceController(Context context) {
+        super(context);
+    }
+
+    @Override
+    public String getPreferenceKey() {
+        return SYSTEM_THEME_STYLE;
+    }
+
+    @Override
+    public boolean isAvailable() {
+        return true;
+    }
+
+    @Override
+    public void displayPreference(PreferenceScreen screen) {
+        super.displayPreference(screen);
+        mSystemThemeStyle = (ListPreference) screen.findPreference(SYSTEM_THEME_STYLE);
+        int systemThemeStyle = Settings.System.getInt(mContext.getContentResolver(),
+                Settings.System.SYSTEM_THEME_STYLE, 0);
+        int valueIndex = mSystemThemeStyle.findIndexOfValue(String.valueOf(systemThemeStyle));
+        mSystemThemeStyle.setValueIndex(valueIndex >= 0 ? valueIndex : 0);
+        mSystemThemeStyle.setSummary(mSystemThemeStyle.getEntry());
+        mSystemThemeStyle.setOnPreferenceChangeListener(this);
+    }
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        if (preference == mSystemThemeStyle) {
+            String value = (String) newValue;
+            Settings.System.putInt(mContext.getContentResolver(), Settings.System.SYSTEM_THEME_STYLE, Integer.valueOf(value));
+            int valueIndex = mSystemThemeStyle.findIndexOfValue(value);
+            mSystemThemeStyle.setSummary(mSystemThemeStyle.getEntries()[valueIndex]);
+        }
+        return true;
+    }
+}
