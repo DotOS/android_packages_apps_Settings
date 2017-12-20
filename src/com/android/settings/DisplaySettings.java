961dba3d1cf6e5b5700fbb5adcd4c6e859a629d6
maxwen <max.weninger@gmail.com>
2017-12-20 21:56:45 +0000

    [2/2] Settings: configure SystemUI theme
    allow to force light or dark or use the wallpaper based
    automatic selection
    
    [Kshitij Gupta]
    - Add a preference controller so as to adapt code for
      Display Settings
    - Refactor some variables. We're theming System, not just
      SystemUI
    
    aex edits:- Disable P's theme selector
    
    Change-Id: Ia2361b9db7009e2a3806b65b22c7e0532e6ebeed
    Signed-off-by: Kshitij Gupta <kshitijgm@gmail.com>

--- src/com/android/settings/DisplaySettings.java
+++ src/com/android/settings/DisplaySettings.java
@@ -33,6 +33,7 @@ import com.android.settings.display.ScreenSaverPreferenceController;
 import com.android.settings.display.ShowOperatorNamePreferenceController;
 import com.android.settings.display.TapToWakePreferenceController;
 import com.android.settings.display.ThemePreferenceController;
+import com.android.settings.display.DarkUIPreferenceController;
 import com.android.settings.display.TimeoutPreferenceController;
 import com.android.settings.display.VrDisplayPreferenceController;
 import com.android.settings.display.WallpaperPreferenceController;
@@ -97,6 +98,7 @@ public class DisplaySettings extends DashboardFragment {
         controllers.add(new ShowOperatorNamePreferenceController(context));
         controllers.add(new WallpaperPreferenceController(context));
         controllers.add(new ThemePreferenceController(context));
+        controllers.add(new DarkUIPreferenceController(context));
         controllers.add(new BrightnessLevelPreferenceController(context, lifecycle));
         controllers.add(new ColorModePreferenceController(context));
         return controllers;
