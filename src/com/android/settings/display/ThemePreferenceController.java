3533cf304d418fcaf4726481023c158271d4442b
Kshitij Gupta <kshitijgm@gmail.com>
2017-12-22 09:41:18 +0000

    ThemePreferenceController: Jump to homescreen on theme change
    Change-Id: Ia7b58c73772c8639832f4c2868b77d24f01c5f47
    Signed-off-by: Kshitij Gupta <kshitijgm@gmail.com>

--- src/com/android/settings/display/ThemePreferenceController.java
+++ src/com/android/settings/display/ThemePreferenceController.java
@@ -13,6 +13,7 @@
  */
 package com.android.settings.display;
 
+import android.content.Intent;
 import android.content.Context;
 import android.content.om.IOverlayManager;
 import android.content.om.OverlayInfo;
@@ -116,6 +117,10 @@ public class ThemePreferenceController extends AbstractPreferenceController impl
         }
         try {
             mOverlayService.setEnabledExclusive((String) newValue, true, UserHandle.myUserId());
+            Intent goHome = new Intent(Intent.ACTION_MAIN);
+            goHome.addCategory(Intent.CATEGORY_HOME);
+            goHome.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            mContext.startActivity(goHome);
         } catch (RemoteException e) {
             return false;
         }
