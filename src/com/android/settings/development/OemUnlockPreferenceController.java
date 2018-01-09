ff9c51c88f25445af619ba046d1a6bdcd8c1ef8b
Bruno Martins <bgcngm@gmail.com>
2018-01-09 15:41:02 +0000

    DevelopmentSettings: Hide OEM unlock by default
     * The "OEM unlocking" toogle should not be visible on any
       device running LineageOS, for obvious reasons. Allow it only
       to show up for Google devices, such as Pixel, that set
       ro.oem_unlock_supported=1.
    
    Change-Id: I2b09974fc24e783b985de213b504e964771cbcb7

--- src/com/android/settings/development/OemUnlockPreferenceController.java
+++ src/com/android/settings/development/OemUnlockPreferenceController.java
@@ -23,6 +23,7 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.res.Resources;
+import android.os.SystemProperties;
 import android.os.UserHandle;
 import android.os.UserManager;
 import android.service.oemlock.OemLockManager;
