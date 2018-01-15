08cc1df18b4724e05d2e62d2e113f60eb38388cf
Shubham Singh <coolsks94@gmail.com>
2018-01-15 16:04:19 +0530

    Settings: Add dotOS Neo theme [2/3]
    Change-Id: I2e8d43cf55213668cac016c4ada5c82eddc689ba
    Signed-off-by: samgrande <talktosayan35@gmail.com>
    Co-authored-by: samgrande <talktosayan35@gmail.com>

--- src/com/android/settings/display/ThemePreferenceController.java
+++ src/com/android/settings/display/ThemePreferenceController.java
@@ -158,7 +158,8 @@ public class ThemePreferenceController extends AbstractPreferenceController impl
                 if (infos.get(i).isEnabled() &&
                         isChangeableOverlay(infos.get(i).packageName) &&
                         !infos.get(i).packageName.equals("com.android.system.theme.dark") &&
-                        !infos.get(i).packageName.equals("com.android.system.theme.black")) {
+                        !infos.get(i).packageName.equals("com.android.system.theme.black") &&
+                        !infos.get(i).packageName.equals("com.android.system.theme.neo")) {
                     return infos.get(i).packageName;
                 }
             }
@@ -189,7 +190,8 @@ public class ThemePreferenceController extends AbstractPreferenceController impl
             for (int i = 0, size = infos.size(); i < size; i++) {
                 if (isChangeableOverlay(infos.get(i).packageName)) {
                     if (!infos.get(i).packageName.equals("com.android.system.theme.dark") && 
-                        !infos.get(i).packageName.equals("com.android.system.theme.black"))
+                        !infos.get(i).packageName.equals("com.android.system.theme.black") &&
+                        !infos.get(i).packageName.equals("com.android.system.theme.neo"))
                         pkgs.add(infos.get(i).packageName);
                 }
             }
