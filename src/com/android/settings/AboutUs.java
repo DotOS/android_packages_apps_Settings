c117daf1386610d2bf8d15db525f03021db3b82c
jhenrique09 <jhenrique09.mcz@hotmail.com>
2018-01-22 17:49:06 +0530

    About us: Rework
     Build okhttp and okio
    
    http://square.github.io/okhttp/
    
    Change-Id: Idf3b080818b81828032f934bef80cb09898da725
    
     Maintainer section: Rework
    
    * Grab from devices.json
    
    Change-Id: I9594c3cb9946bc72338c793288aaff52bbd6f301
    Signed-off-by: Shubham Singh <coolsks94@gmail.com>
    Signed-off-by: Mohan Cm <mohanmanjappa@gmail.com>

--- src/com/android/settings/AboutUs.java
+++ src/com/android/settings/AboutUs.java
@@ -34,27 +34,6 @@ public class AboutUs extends SettingsPreferenceFragment {
 
         addPreferencesFromResource(R.xml.about_us);
 
-        PreferenceCategory maintainers = (PreferenceCategory)findPreference("maintainers");
-
-        String[] maintainers_title = getResources().getStringArray(R.array.maintainers_title);
-        String[] maintainers_devices = getResources().getStringArray(R.array.maintainers_devices);
-        String[] maintainers_url = getResources().getStringArray(R.array.maintainers_url);
-
-        for (int i = 0; i < maintainers_title.length; i++) {
-            Preference maintainer = new Preference(getPrefContext());
-            final String maintainer_url = maintainers_url[i];
-            maintainer.setIcon(R.drawable.ic_devs_phone);
-            maintainer.setTitle(maintainers_title[i]);
-            //maintainer.setSummary(String.format(getString(R.string.maintainers_url), maintainers_devices[i]));
-            maintainer.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
-                @Override
-                public boolean onPreferenceClick(Preference preference) {
-                    getActivity().startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(maintainer_url)));
-                    return true;
-                }
-            });
-            maintainers.addPreference(maintainer);
-        }
     }
 
     @Override
