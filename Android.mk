0aca9e3acfe185d8d72bf3bbfda90e854033764c
ganeshi4u <ganeshvarmai4u@gmail.com>
2018-03-01 18:45:13 +0530

    [WIP]DotExtras:bring in dotextras navbar
    Change-Id: I2cc8c741b3512baa32824d6d14a27d2f38f7b7b2
    Signed-off-by: ganeshi4u <ganeshvarmai4u@gmail.com>

--- Android.mk
+++ Android.mk
@@ -18,14 +18,18 @@ LOCAL_MODULE_TAGS := optional
 LOCAL_USE_AAPT2 := true
 
 LOCAL_SRC_FILES := $(call all-java-files-under, src) \
-                   $(call all-java-files-under, ../DotExtras/src)
+                   $(call all-java-files-under, ../DotExtras/src) \
+		   $(call all-java-files-under, ../DotExtras-nav/src)
 
 LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res \
     frameworks/support/v7/preference/res \
     frameworks/support/v14/preference/res \
     frameworks/support/v7/appcompat/res \
     frameworks/support/v7/recyclerview/res \
-    packages/apps/DotExtras/res
+    frameworks/support/v7/cardview/res \
+    frameworks/support/design/res \
+    packages/apps/DotExtras/res \
+    packages/apps/DotExtras-nav/res
 
 LOCAL_STATIC_ANDROID_LIBRARIES := \
     android-support-v4 \
@@ -34,7 +38,10 @@ LOCAL_STATIC_ANDROID_LIBRARIES := \
     android-support-v7-cardview \
     android-support-v7-preference \
     android-support-v7-recyclerview \
-    android-support-v14-preference
+    android-support-v14-preference \
+    android-support-design 
+
+LOCAL_STATIC_JAVA_AAR_LIBRARIES:= bottomnav 
 
 LOCAL_JAVA_LIBRARIES := \
     bouncycastle \
@@ -49,13 +56,19 @@ LOCAL_STATIC_JAVA_LIBRARIES := \
 
 LOCAL_PROGUARD_FLAG_FILES := proguard.flags
 
+LOCAL_FULL_LIBS_MANIFEST_FILES := $(LOCAL_PATH)/AndroidManifest-DE.xml
+
 LOCAL_AAPT_FLAGS := --auto-add-overlay \
     --extra-packages android.support.v7.preference \
     --extra-packages android.support.v14.preference \
     --extra-packages android.support.v17.preference \
     --extra-packages android.support.v7.appcompat \
     --extra-packages android.support.v7.recyclerview \
-    --extra-packages com.dot.dotextras
+    --extra-packages android.support.v7.cardview \
+    --extra-packages android.support.design \
+    --extra-packages com.aurelhubert.ahbottomnavigation \
+    --extra-packages com.dot.dotextras \
+    --extra-packages com.droidontime.dotextras
 
 ifneq ($(INCREMENTAL_BUILDS),)
     LOCAL_PROGUARD_ENABLED := disabled
@@ -67,6 +80,13 @@ include frameworks/opt/setupwizard/library/common-gingerbread.mk
 include frameworks/base/packages/SettingsLib/common.mk
 
 include $(BUILD_PACKAGE)
+
+include $(CLEAR_VARS)
+
+LOCAL_PREBUILT_STATIC_JAVA_LIBRARIES := \
+ 	bottomnav:libs/bottomnav.aar 
+ 
+include $(BUILD_MULTI_PREBUILT)
 
 # Use the following include to make our test apk.
 ifeq (,$(ONE_SHOT_MAKEFILE))
