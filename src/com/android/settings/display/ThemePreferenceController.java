eb4abdd4f7cbe4ba04ffca5ad2d61e5257e027ee
Bauuuuu <abansode@outlook.com>
2018-03-13 00:53:52 +0100

    fix derps
    Change-Id: Icf2214ad1fa609547b6b9a21bba7d3bfc218509b

--- src/com/android/settings/display/ThemePreferenceController.java
+++ src/com/android/settings/display/ThemePreferenceController.java
@@ -13,7 +13,6 @@
  */
 package com.android.settings.display;
 
-import android.content.Intent;
 import android.content.Context;
 import android.content.om.IOverlayManager;
 import android.content.om.OverlayInfo;
@@ -27,7 +26,6 @@ import android.support.annotation.VisibleForTesting;
 import android.support.v7.preference.ListPreference;
 import android.support.v7.preference.Preference;
 import android.text.TextUtils;
-import android.widget.Toast;
 
 import com.android.settings.R;
 import com.android.settings.core.PreferenceControllerMixin;
@@ -124,12 +122,6 @@ public class ThemePreferenceController extends AbstractPreferenceController impl
         }
         try {
             mOverlayService.setEnabledExclusive((String) newValue, true, UserHandle.myUserId());
-            Toast.makeText(mContext, mContext.getString(R.string.theme_applied_toast),
-                Toast.LENGTH_LONG).show();
-            Intent goHome = new Intent(Intent.ACTION_MAIN);
-            goHome.addCategory(Intent.CATEGORY_HOME);
-            goHome.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-            mContext.startActivity(goHome);
         } catch (RemoteException e) {
             return false;
         }
