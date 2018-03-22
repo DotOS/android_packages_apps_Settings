0386db530de28fed138f77059fe1dc10a6ceb8a2
Shubham Singh <coolsks94@gmail.com>
2018-03-23 02:30:33 +0530

    Add libraries needed for DotOS Stats
       * retrofit
       * converter-gson
       * rxjava
    
    Change-Id: Ia32db6f39b2c954c0d6595ddd12b4b81f95d06ba
    Signed-off-by: Shubham Singh <coolsks94@gmail.com>

--- Android.mk
+++ Android.mk
@@ -53,7 +53,16 @@ LOCAL_STATIC_JAVA_LIBRARIES := \
     jsr305 \
     settings-logtags \
     okhttpcustom \
-    okio
+    okio \
+    retrofit \
+    converter-gson \
+    rxjava \
+    adapter-rxjava \
+    gson \
+    reactive-streams
+
+LOCAL_STATIC_JAVA_AAR_LIBRARIES += \
+    rxandroid
 
 LOCAL_PROGUARD_FLAG_FILES := proguard.flags
 
@@ -63,7 +72,8 @@ LOCAL_AAPT_FLAGS := --auto-add-overlay \
     --extra-packages android.support.v17.preference \
     --extra-packages android.support.v7.appcompat \
     --extra-packages android.support.v7.recyclerview \
-    --extra-packages com.dot.dotextras
+    --extra-packages com.dot.dotextras \
+    --extra-packages io.reactivex.android
 
 ifneq ($(INCREMENTAL_BUILDS),)
     LOCAL_PROGUARD_ENABLED := disabled
@@ -80,7 +90,14 @@ include $(CLEAR_VARS)
 
 LOCAL_PREBUILT_STATIC_JAVA_LIBRARIES := \
     okhttpcustom:libs/okhttp-3.8.1.jar \
-    okio:libs/okio-1.13.0.jar
+    okio:libs/okio-1.13.0.jar \
+    retrofit:libs/retrofit-2.4.0.jar \
+    converter-gson:libs/converter-gson-2.4.0.jar \
+    rxjava:libs/rxjava-2.1.11.jar \
+    adapter-rxjava:libs/adapter-rxjava2-2.4.0.jar \
+    rxandroid:libs/rxandroid-2.0.2.aar \
+    gson:libs/gson-2.8.2.jar \
+    reactive-streams:libs/reactive-streams-1.0.2.jar
 
 include $(BUILD_MULTI_PREBUILT)
 
