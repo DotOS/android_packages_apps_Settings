a773c8619c35647f143b9e5d4a8d76fd3193cb5c
Alex Cruz <du.alexcruz@gmail.com>
2018-04-17 21:28:11 -0500

    Add a preference controller for Smart Pixels
    This keeps things uniformed with other features that we have in settings.
    Plus this avoids an NPE that's caused by checking for a preference in DT
    while the actual preference is in Settings.
    
    Change-Id: I171c0f0b962c2219d7ae0bd5c8a1d7d58d8cbb9c

--- src/com/android/settings/fuelgauge/PowerUsageSummary.java
+++ src/com/android/settings/fuelgauge/PowerUsageSummary.java
@@ -282,6 +282,7 @@ public class PowerUsageSummary extends PowerUsageBase implements OnLongClickList
                 BatteryTipListener */);
         controllers.add(mBatteryTipPreferenceController);
         controllers.add(new BatteryPercentagePreferenceController(context));
+        controllers.add(new SmartPixelsPreferenceController(context));
         return controllers;
     }
 
