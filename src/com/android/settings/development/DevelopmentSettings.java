fb15e8581b1963e5d85418ce075e80652112530d
Bauuuuu <abansode@outlook.com>
2018-05-14 14:05:14 +0200

    Revert "Settings: Introduce Substratum changes."
    This reverts commit 8ae7efca3463e65a687adeffe3aac850f779ca54.
    
    Change-Id: I1ac79f3e15496e3c63e78741f7c8e33ee04c99b2

--- src/com/android/settings/development/DevelopmentSettings.java
+++ src/com/android/settings/development/DevelopmentSettings.java
@@ -247,8 +247,6 @@ public class DevelopmentSettings extends RestrictedSettingsFragment
 
     private static final String KEY_CONVERT_FBE = "convert_to_file_encryption";
 
-    private static final String FORCE_AUTHORIZE_SUBSTRATUM_PACKAGES = "force_authorize_substratum_packages";
-
     private static final int RESULT_DEBUG_APP = 1000;
     private static final int RESULT_MOCK_LOCATION_APP = 1001;
 
@@ -273,9 +271,6 @@ public class DevelopmentSettings extends RestrictedSettingsFragment
     private boolean mHaveDebugSettings;
     private boolean mDontPokeProperties;
     private EnableAdbPreferenceController mEnableAdbController;
-
-    private SwitchPreference mForceAuthorizeSubstratumPackages;
-
     private Preference mClearAdbKeys;
     private SwitchPreference mEnableTerminal;
     private SwitchPreference mAdbOverNetwork;
@@ -461,13 +456,11 @@ public class DevelopmentSettings extends RestrictedSettingsFragment
         mForceAllowOnExternal = findAndInitSwitchPref(FORCE_ALLOW_ON_EXTERNAL_KEY);
         mPassword = findPreference(LOCAL_BACKUP_PASSWORD);
         mAllPrefs.add(mPassword);
-        mForceAuthorizeSubstratumPackages = findAndInitSwitchPref(FORCE_AUTHORIZE_SUBSTRATUM_PACKAGES);
 
         if (!mUm.isAdminUser()) {
             disableForUser(mClearAdbKeys);
             disableForUser(mEnableTerminal);
             disableForUser(mPassword);
-            disableForUser(mForceAuthorizeSubstratumPackages);
         }
 
         mDebugAppPref = findPreference(DEBUG_APP_KEY);
@@ -869,18 +862,6 @@ public class DevelopmentSettings extends RestrictedSettingsFragment
         updateBluetoothDisableAbsVolumeOptions();
         updateBluetoothEnableInbandRingingOptions();
         updateBluetoothA2dpConfigurationValues();
-        updateForceAuthorizeSubstratumPackagesOptions();
-    }
-
-    private void writeForceAuthorizeSubstratumPackagesOptions() {
-        Settings.Secure.putInt(getActivity().getContentResolver(),
-                Settings.Secure.FORCE_AUTHORIZE_SUBSTRATUM_PACKAGES,
-                mForceAuthorizeSubstratumPackages.isChecked() ? 1 : 0);
-    }
-
-    private void updateForceAuthorizeSubstratumPackagesOptions() {
-        mForceAuthorizeSubstratumPackages.setChecked(Settings.Secure.getInt(getActivity().getContentResolver(),
-                Settings.Secure.FORCE_AUTHORIZE_SUBSTRATUM_PACKAGES, 0) != 0);
     }
 
     private void resetDangerousOptions() {
@@ -2589,8 +2570,6 @@ public class DevelopmentSettings extends RestrictedSettingsFragment
             writeForceResizableOptions();
         } else if (preference == mBluetoothShowDevicesWithoutNames) {
             writeBluetoothShowDevicesWithoutUserFriendlyNameOptions();
-        } else if (preference == mForceAuthorizeSubstratumPackages) {
-            writeForceAuthorizeSubstratumPackagesOptions();
         } else if (preference == mBluetoothDisableAbsVolume) {
             writeBluetoothDisableAbsVolumeOptions();
         } else if (preference == mBluetoothEnableInbandRinging) {
