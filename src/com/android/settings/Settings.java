678e296506b854fdb0a64e7e5756ba257e9b0bd4
bigrushdog <randall.rushing@gmail.com>
2018-10-06 19:12:07 -0700

    Initial checkin for SmartNav navigation suite [2/4]
    Change-Id: I4b68d92e3bf3e6e5653d964e43e616ce4ca94d7a
    Signed-off-by: Shubham Singh <coolsks94@gmail.com>

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -168,5 +168,10 @@ public class Settings extends SettingsActivity {
 
     // dotEXTRAS
     public static class DotExtrasSettingsActivity extends SettingsActivity {}
+    // SmartNav
+    public static class NavigationSettingsActivity extends SettingsActivity {}
+    public static class SmartbarSettingsActivity extends SettingsActivity {}
+    public static class FlingSettingsActivity extends SettingsActivity {}
+    public static class PulseSettingsActivity extends SettingsActivity {}
 
 }
