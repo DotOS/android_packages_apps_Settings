f7f57c4a9fee535d70daa082ce5dcb46519434f6
IacobIonut01 <paulionut2003@gmail.com>
2018-10-15 17:40:30 +0300

    Settings UI : Show User Icon in dashboard
    Change-Id: Id02b6375087c3187a4d13388d5571b72184a01d1
    Signed-off-by: IacobIonut01 <paulionut2003@gmail.com>

---
+++ src/com/android/settings/dashboard/DashboardProfileIcon.java
@@ -0,0 +1,34 @@
+package com.android.settings.dashboard;
+
+import android.content.Context;
+import android.os.UserManager;
+import android.util.TypedValue;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.content.pm.UserInfo;
+
+import com.android.settingslib.Utils;
+
+public class DashboardProfileIcon extends RelativeLayout {
+
+    public DashboardProfileIcon(Context context) {
+        super(context);
+		init(context);
+    }
+
+    private void init(Context context) {
+        UserManager userManager = (UserManager) context.getSystemService(Context.USER_SERVICE);
+        UserInfo info = Utils.getExistingUser(userManager, android.os.Process.myUserHandle());
+        LayoutParams lp = new LayoutParams((int) dp(46), (int) dp(46));
+        setLayoutParams(lp);
+        ImageView icon = new ImageView(context);
+        icon.setLayoutParams(lp);
+        icon.setScaleType(ImageView.ScaleType.FIT_CENTER);
+        icon.setImageDrawable(Utils.getUserIcon(context, userManager, info));
+        addView(icon);
+    }
+
+    private float dp(int dp) {
+        return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp, getResources().getDisplayMetrics());
+    }
+}
