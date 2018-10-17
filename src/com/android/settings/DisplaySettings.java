6e6eeef42eb4241eb23adb76e6d005441d4fbd62
calebcabob <calphonic@gmail.com>
2018-10-16 20:52:49 -0500

    Introduce QS tile style picker [3/3]
    Change-Id: I78b4bf3f7ee34a878614fb2c14e00537b3594909

--- src/com/android/settings/DisplaySettings.java
+++ src/com/android/settings/DisplaySettings.java
@@ -37,6 +37,7 @@ import com.android.settings.display.ShowOperatorNamePreferenceController;
 import com.android.settings.display.TapToWakePreferenceController;
 import com.android.settings.display.ThemePreferenceController;
 import com.android.settings.display.DarkUIPreferenceController;
+import com.android.settings.display.QsTileStylePreferenceController;
 import com.android.settings.display.TimeoutPreferenceController;
 import com.android.settings.display.VrDisplayPreferenceController;
 import com.android.settings.display.WallpaperPreferenceController;
@@ -106,6 +107,7 @@ public class DisplaySettings extends DashboardFragment {
         controllers.add(new ShowOperatorNamePreferenceController(context));
         controllers.add(new WallpaperPreferenceController(context));
         controllers.add(new ThemePreferenceController(context));
+        controllers.add(new QsTileStylePreferenceController(context));
         controllers.add(mUIStylePreference = new DarkUIPreferenceController(context, lifecycle, fragment));
         controllers.add(new ForceAllowThemePreferenceController(context, mUIStylePreference, mAccentPickerPreference));
         controllers.add(new BrightnessLevelPreferenceController(context, lifecycle));
