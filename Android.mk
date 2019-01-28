allianceSparx/packages_apps_Settings

settings: Initial checkin for SmartNav navigation suite [2/4]
Change-Id: I4b68d92e3bf3e6e5653d964e43e616ce4ca94d7a

Android.mk
@@ -20,6 +20,9 @@ LOCAL_USE_AAPT2 := true
 
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
 LOCAL_SRC_FILES += $(call all-java-files-under, ../WolvesDen/src)
+LOCAL_SRC_FILES += $(call all-java-files-under, ../SmartNavSettings/src)
+
+LOCAL_FULL_LIBS_MANIFEST_FILES += $(LOCAL_PATH)/AndroidManifest-SmartNav.xml
 
 LOCAL_STATIC_ANDROID_LIBRARIES := \
     android-slices-builders \
@@ -52,7 +55,8 @@ LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res \
     frameworks/support/v14/preference/res \
     frameworks/support/v7/appcompat/res \
     frameworks/support/v7/recyclerview/res \
-    packages/apps/WolvesDen/res
+    packages/apps/WolvesDen/res \
+    packages/apps/SmartNavSettings/res
 
 LOCAL_PROGUARD_FLAG_FILES := proguard.flags
 

DotOS/android_packages_apps_Settings

Signed-off-by: Sipun Ku Mahanta <sipunkumar85@gmail.com>
Change-Id: I736866293ae4b9fb13e8b16ffb7db98035ffa3e8

Android.mk
@@ -19,14 +19,18 @@ LOCAL_MODULE_TAGS := optional
 LOCAL_USE_AAPT2 := true
 
 LOCAL_SRC_FILES := $(call all-java-files-under, src) \
-                   $(call all-java-files-under, ../DotExtras/src)
+                   $(call all-java-files-under, ../DotExtras/src) \
+                   $(call all-java-files-under, ../SmartNavSettings/src)
+
+LOCAL_FULL_LIBS_MANIFEST_FILES += $(LOCAL_PATH)/AndroidManifest-SmartNav.xml
 
 LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res \
     frameworks/support/v7/preference/res \
     frameworks/support/v14/preference/res \
     frameworks/support/v7/appcompat/res \
     frameworks/support/v7/recyclerview/res \
-    packages/apps/DotExtras/res
+    packages/apps/DotExtras/res \
+    packages/apps/SmartNavSettings/res
 
 LOCAL_STATIC_ANDROID_LIBRARIES := \
     android-slices-builders \