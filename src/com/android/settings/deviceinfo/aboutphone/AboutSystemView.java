be6644a980f37913224b56e6db078aea2589463c
IacobIonut01 <paulionut2003@gmail.com>
2019-04-12 15:46:03 +0300

    Implement new Device Info UI
    Signed-off-by: IacobIonut01 <paulionut2003@gmail.com>
    Change-Id: Ia2c9ac936b06546037089dc500cea229a4ba36e6

---
+++ src/com/android/settings/deviceinfo/aboutphone/AboutSystemView.java
@@ -0,0 +1,53 @@
+package com.android.settings.deviceinfo.aboutphone;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.content.pm.UserInfo;
+import android.os.UserManager;
+
+import com.android.settings.R;
+import com.android.settingslib.Utils;
+
+import android.support.annotation.Nullable;
+
+import android.support.design.widget.TabLayout;
+
+public class AboutSystemView extends LinearLayout {
+
+    TabLayout tabLayout;
+
+    public AboutSystemView(Context context) {
+        super(context);
+        init(context);
+    }
+
+    public AboutSystemView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        init(context);
+    }
+
+    public AboutSystemView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init(context);
+    }
+
+    private void init(Context context) {
+        View view = inflate(context, R.layout.dot_about_system, this);
+        TextView profile_name = view.findViewById(R.id.profile_name);
+        ImageView avatar = view.findViewById(R.id.avatar_icon);
+        tabLayout = view.findViewById(R.id.system_Sections);
+        UserManager userManager = (UserManager) context.getSystemService(Context.USER_SERVICE);
+        UserInfo info = com.android.settings.Utils.getExistingUser(userManager, android.os.Process.myUserHandle());
+        avatar.setImageDrawable(Utils.getUserIcon(context, userManager, info));
+        profile_name.setText(info.name);
+    }
+
+    public void setOnPositionChangeListener(TabLayout.BaseOnTabSelectedListener listener) {
+        tabLayout.addOnTabSelectedListener(listener);
+    }
+
+}
