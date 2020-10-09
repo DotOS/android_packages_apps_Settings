a932f19c9f7a5d650e00a6b8c025f5bb32519e97
IacobIonut01 <paulionut2003@gmail.com>
2020-10-09 08:22:53 +0300

    fuelgauge : Pause or cancel animation when fragment is destroyed prematurely when BatteryMeterView animation ends.
    Signed-off-by: IacobIonut01 <paulionut2003@gmail.com>

--- src/com/android/settings/fuelgauge/PowerUsageSummary.java
+++ src/com/android/settings/fuelgauge/PowerUsageSummary.java
@@ -115,6 +115,8 @@ public class PowerUsageSummary extends PowerUsageBase implements OnLongClickList
     boolean mNeedUpdateBatteryTip;
     @VisibleForTesting
     BatteryTipPreferenceController mBatteryTipPreferenceController;
+    @VisibleForTesting
+    ValueAnimator animator;
 
     @VisibleForTesting
     final ContentObserver mSettingsObserver = new ContentObserver(new Handler()) {
@@ -267,6 +269,7 @@ public class PowerUsageSummary extends PowerUsageBase implements OnLongClickList
     @Override
     public void onPause() {
         getContentResolver().unregisterContentObserver(mSettingsObserver);
+        detachBatteryHeaderAnimationIfNecessary();
         super.onPause();
     }
 
@@ -412,7 +415,7 @@ public class PowerUsageSummary extends PowerUsageBase implements OnLongClickList
         mBatteryLevel = currentLevel;
         final int diff = Math.abs(prevLevel - currentLevel);
         if (diff != 0) {
-            final ValueAnimator animator = ValueAnimator.ofInt(prevLevel, currentLevel);
+            animator = ValueAnimator.ofInt(prevLevel, currentLevel);
             animator.setDuration(BATTERY_ANIMATION_DURATION_MS_PER_LEVEL * diff);
             animator.setInterpolator(AnimationUtils.loadInterpolator(getContext(),
                     android.R.interpolator.fast_out_slow_in));
@@ -426,6 +429,13 @@ public class PowerUsageSummary extends PowerUsageBase implements OnLongClickList
                 }
             });
             animator.start();
+        }
+    }
+
+    @VisibleForTesting
+    void detachBatteryHeaderAnimationIfNecessary() {
+        if (animator.isRunning()) {
+            animator.pause();
         }
     }
 
