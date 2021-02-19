2fe821b3145dffdcdd3c82fce629a217ddad7f59
IacobIonut01 <paulionut2003@gmail.com>
2021-02-19 12:55:20 +0000

    BaseLayout: Implement CollapsingToolbarLayout
    Ported from A12-DP1
    
    Signed-off-by: IacobIonut01 <paulionut2003@gmail.com>
    Change-Id: I58d2b359cde56ff9f7ed75209673a17677ad9f12

--- src/com/android/settings/core/SettingsBaseActivity.java
+++ src/com/android/settings/core/SettingsBaseActivity.java
@@ -43,6 +43,7 @@ import com.android.settings.SubSettings;
 import com.android.settings.dashboard.CategoryManager;
 import com.android.settingslib.drawer.Tile;
 
+import com.google.android.material.appbar.AppBarLayout;
 import com.google.android.setupcompat.util.WizardManagerHelper;
 
 import java.util.ArrayList;
@@ -87,6 +88,7 @@ public class SettingsBaseActivity extends FragmentActivity {
         final Toolbar toolbar = findViewById(R.id.action_bar);
         if (theme.getBoolean(android.R.styleable.Theme_windowNoTitle, false)) {
             toolbar.setVisibility(View.GONE);
+            ((AppBarLayout) findViewById(R.id.baseAppBar)).setExpanded(false);
             return;
         }
         setActionBar(toolbar);
