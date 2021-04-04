40ccb6482b6476199a3de475d9a66f635aac59bf
IacobIonut01 <paulionut2003@gmail.com>
2021-04-04 21:13:02 +0000

    Settings Dashboard : Redesign to dotOS 5.1 guidelines
    Signed-off-by: IacobIonut01 <paulionut2003@gmail.com>
    Change-Id: I8de9210f25dfb32ac8a3e74e17d738c99551861f

--- src/com/android/settings/homepage/TopLevelSettings.java
+++ src/com/android/settings/homepage/TopLevelSettings.java
@@ -81,6 +81,19 @@ public class TopLevelSettings extends DashboardFragment implements
     }
 
     @Override
+    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+        super.onCreatePreferences(savedInstanceState, rootKey);
+        for (int i = 0; i < getPreferenceScreen().getPreferenceCount(); i++) {
+            Preference pref = getPreferenceScreen().getPreference(i);
+            if (pref.isVisible() && pref.getTitle() != null && 
+                pref.getLayoutResource() != R.layout.dot_preference_dashboard_top && 
+                pref.getLayoutResource() != R.layout.dot_preference_dashboard_bottom) {
+                pref.setLayoutResource(R.layout.dot_preference_dashboard_middle);
+            }
+        }
+    }
+
+    @Override
     public boolean onPreferenceStartFragment(PreferenceFragmentCompat caller, Preference pref) {
         new SubSettingLauncher(getActivity())
                 .setDestination(pref.getFragment())
