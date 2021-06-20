7c5b749e99759b1f68e370812b27d2aee44c05f5
IacobIonut01 <paulionut2003@gmail.com>
2021-06-20 10:01:35 +0000

    [5.1.3-Bugfix] PowerUsageSummary: Handle NPE when animator is null
    Sometimes with low battery level, this NPE is thrown
    so we chek if animator is not null before checking
    if it's already running, avoiding the NPE.
    
    Signed-off-by: IacobIonut01 <paulionut2003@gmail.com>
    Change-Id: I028a2f95332ec6c56e3ccfd1a78bf831f19ea1ed

--- src/com/android/settings/fuelgauge/PowerUsageSummary.java
+++ src/com/android/settings/fuelgauge/PowerUsageSummary.java
@@ -452,7 +452,7 @@ public class PowerUsageSummary extends PowerUsageBase implements OnLongClickList
 
     @VisibleForTesting
     void detachBatteryHeaderAnimationIfNecessary() {
-        if (animator.isRunning()) {
+        if (animator != null && animator.isRunning()) {
             animator.pause();
         }
     }
